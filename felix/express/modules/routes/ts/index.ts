ec95b4d2a289aed545525151e9c4604331edc098
ftovar8 <felix.godo@gmail.com>
2022-12-23 18:28:14 -0400

    fix: update code

--- felix/express/modules/routes/ts/index.ts
+++ felix/express/modules/routes/ts/index.ts
@@ -1,7 +1,3 @@
-import {server} from '@tests/express-app/server';
-
-console.log(1, server)
-
 export/*bundle*/ function routes(app) {
     app.get('/', (req, res) => {
         res.send('Express page with BeyondJS ')
