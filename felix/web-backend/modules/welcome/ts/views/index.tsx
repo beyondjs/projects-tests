66dc017f146b2b35ac7bf4f9a1f4a63bf4938552
ftovar8 <felix.godo@gmail.com>
2023-01-02 09:53:39 -0400

    update code

--- felix/web-backend/modules/welcome/ts/views/index.tsx
+++ felix/web-backend/modules/welcome/ts/views/index.tsx
@@ -1,16 +1,15 @@
 import * as React from "react";
-import {BeyondIcon} from "@bgroup/ui/icon";
-import {BeyondEmpty} from '@bgroup/ui/empty';
 import {BeyondSpinner} from '@bgroup/ui/spinner';
+import Swal from "sweetalert2";
 
 export /*bundle*/
 function Page(): JSX.Element {
+
+    console.log(new Swal())
     return (
         <div className="page__container">
             <BeyondSpinner color="#617096" className="spinner"/>
             <h1>Welcome <span className="beyond">BeyondJS</span>!</h1>
-            <BeyondIcon icon="delete"/>
-            <BeyondEmpty text="No records to display" icon="info"/>
         </div>
     );
 }
