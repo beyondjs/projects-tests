3488aa02d8ffd517c72f5dd8801a472e73937f3d
ftovar8 <felix.godo@gmail.com>
2023-01-13 15:17:25 -0400

    code

---
+++ felix/web-backend/modules/svelte/svelte/widget.svelte
@@ -0,0 +1,10 @@
+<script>
+    console.log('svelte widget')
+    let count = 0;
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<h3>Svelte counter: {count}</h3>
+<button on:click={handleClick}>click</button>
---
+++ felix/web-backend/modules/vue/vue/widget.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>Vue counter: {{ result }}</h3>
+    <button @click='add'>click</button>
+  </div>
+</template>
+
+<script lang="ts">
+console.log('vue widget')
+export default {
+  name: "exampleVue",
+  data() {
+    return {
+      result: 0
+    }
+  },
+  props: ['value'],
+  methods: {
+    emitResult() {
+      this.$emit('input', this.result)
+    },
+    add() {
+      this.result += 1
+      this.emitResult()
+    },
+    sub() {
+      this.result -= 1
+      this.emitResult()
+    }
+  }
+}
+</script>
